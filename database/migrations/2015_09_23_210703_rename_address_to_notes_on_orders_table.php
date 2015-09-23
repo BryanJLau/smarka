d181a21dc644dcf1684f1f8499ab54c53b301737
Bryan Lau <bryan.d.lau@outlook.com>
2015-09-23 14:17:51 -0700

    Renamed address to special notes for orders

---
+++ database/migrations/2015_09_23_210703_rename_address_to_notes_on_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameAddressToNotesOnOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('orders', function(Blueprint $table)
+		{
+			//
+			$table->renameColumn('address', 'notes');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('orders', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
