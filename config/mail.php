5a19cc461bb72e27a1b92b7ebc6b990e4e1809fa
Bryan Lau <bryan.d.lau@outlook.com>
2015-09-25 23:06:00 -0700

    Moved email details to environment variables

--- config/mail.php
+++ config/mail.php
@@ -18,7 +18,7 @@ return [
 	|
 	*/
 
-	'driver' => env('MAIL_DRIVER', 'smtp'),
+	'driver' => getenv('MAIL_DRIVER') || env('MAIL_DRIVER', 'smtp'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ return [
 	|
 	*/
 
-	'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+	'host' => getenv('MAIL_HOST') || env('MAIL_HOST', 'smtp.mailgun.org'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ return [
 	|
 	*/
 
-	'port' => env('MAIL_PORT', 587),
+	'port' => getenv('MAIL_PORT') || env('MAIL_PORT', 587),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -83,7 +83,7 @@ return [
 	|
 	*/
 
-	'username' => env('MAIL_USERNAME'),
+	'username' => getenv('MAIL_USERNAME') || env('MAIL_USERNAME'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -96,7 +96,7 @@ return [
 	|
 	*/
 
-	'password' => env('MAIL_PASSWORD'),
+	'password' => getenv('MAIL_PASSWORD') || env('MAIL_PASSWORD'),
 
 	/*
 	|--------------------------------------------------------------------------
